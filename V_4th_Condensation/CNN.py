9daba3df9bf9806dcefbbc7be10bba9c0fea4a03
Khalil Wong <1039906090@qq.com>
2020-06-18 21:34:34 +0800

    test model

--- V_4th_Condensation/CNN.py
+++ V_4th_Condensation/CNN.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as mpl
 
 ################################################################################
-def ReadFile(FileName):
+def ReadFile1(FileName):
     #
     with open(FileName, 'r') as In:
         Data = In.readlines()
@@ -70,6 +70,71 @@ def ReadFile(FileName):
     Adsorbed = np.array(Adsorbed)
     #
     return(ID, Tt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed)
+
+################################################################################
+def ReadFile2(FileName):
+    #
+    with open(FileName, 'r') as In:
+        Data = In.readlines()
+        InData = Data[1:]
+        ID = []
+        Tt = []
+        CN = []
+        TmVt = []
+        X = []
+        Y = []
+        Z = []
+        VX = []
+        VY = []
+        VZ = []
+        FX = []
+        FY = []
+        FZ = []
+        FVX = []
+        FVY = []
+        FVZ = []
+        Adsorbed = []
+        for pdata in InData:
+            (id, tt, cn, tmvt, x, y, z, vx, vy, vz, fx, fy, fz, fvx, fvy, fvz, ad) = pdata.split('\t', 16)
+            ID.append(int(id))
+            Tt.append(int(tt))
+            CN.append(int(cn))
+            TmVt.append(int(tmvt))
+            X.append(float(x))
+            Y.append(float(y))
+            Z.append(float(z))
+            VX.append(float(vx))
+            VY.append(float(vy))
+            VZ.append(float(vz))
+            FX.append(float(fx))
+            FY.append(float(fy))
+            FZ.append(float(fz))
+            FVX.append(float(fvx))
+            FVY.append(float(fvy))
+            FVZ.append(float(fvz))
+            if (ad == 'False\n'):
+                Adsorbed.append(0)
+            elif (ad == 'True\n'):
+                Adsorbed.append(1)
+    ID = np.array(ID)
+    Tt = np.array(Tt)
+    CN = np.array(CN)
+    TmVt = np.array(TmVt)
+    X = np.array(X)
+    Y = np.array(Y)
+    Z = np.array(Z)
+    VX = np.array(VX)
+    VY = np.array(VY)
+    VZ = np.array(VZ)
+    FX = np.array(FX)
+    FY = np.array(FY)
+    FZ = np.array(FZ)
+    FVX = np.array(FVX)
+    FVY = np.array(FVY)
+    FVZ = np.array(FVZ)
+    Adsorbed = np.array(Adsorbed)
+    #
+    return(ID, Tt, CN, TmVt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed)
 
 ################################################################################
 class Net(nn.Module):
@@ -156,8 +221,8 @@ def Learning_Plot(ur, vr, wr, output, Type):
             ax.set_ylabel('$f$')
             #ax.set_title('p0 = %f;p1 = %f'%(p[0], p[1]))
             mpl.savefig('CNN_v_'+ Type +'.png', dpi = 600)
-        mpl.show()
-        #mpl.close()
+        #mpl.show()
+        mpl.close()
     #
     fig, ax = mpl.subplots()
     #levels = np.arange(0.0, 1.05, 0.05)
@@ -170,14 +235,15 @@ def Learning_Plot(ur, vr, wr, output, Type):
     ax.set_ylabel('$f$')
     #ax.set_title('p0 = %f;p1 = %f'%(p[0], p[1]))
     mpl.savefig('CNN_w_'+ Type +'.png', dpi = 600)
-    mpl.show()
-    #mpl.close()
+    #mpl.show()
+    mpl.close()
 
 ################################################################################
 def main():
     #
+    '''
     FileName = 'Incident_Reflection.data'
-    ID, Tt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed = ReadFile(FileName)
+    ID, Tt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed = ReadFile1(FileName)
     UnA_N = 0
     for i in range(len(Adsorbed)):
         if Adsorbed[i] == 0:
@@ -217,10 +283,45 @@ def main():
         #print(torch.cuda.memory_summary())
         optimizer.step()
     #
+    PATH = './distribution_net.pth'
+    torch.save(net.state_dict(), PATH)
     output = Prediction.to("cpu", torch.double)
     output = output.view(3, 31)
     output_np = output.detach().numpy()
     Learning_Plot(ur, vr, wr, output_np, 'Prediction')
+    #
+    '''
+    #
+    FileName = 'Incident_Reflection_Test.data'
+    ID, Tt, CN, TmVt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed = ReadFile2(FileName)
+    UnA_N = 0
+    for i in range(len(Adsorbed)):
+        if Adsorbed[i] == 0:
+            UnA_N += 1
+    #
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #
+    Test_np = np.concatenate((VX, VY, VZ), axis = 0)
+    Test_np[0:100000] = 1.0
+    Test_np[100000:200000] = 1.0
+    Test_np[200000:300000] = np.sqrt(2.0)
+    Test = torch.from_numpy(Test_np)
+    Test = Test.view(1, 3, -1)
+    Test_d = Test.to(device, dtype=torch.float)
+    I = 30
+    ur, Targetx = Reflection_Distribution(UnA_N, FVX, Adsorbed, I, -2.0, 2.0)
+    vr, Targety = Reflection_Distribution(UnA_N, FVY, Adsorbed, I, -2.0, 2.0)
+    wr, Targetz = Reflection_Distribution(UnA_N, FVZ, Adsorbed, I, 0.0, 2.0)
+    #
+    with torch.no_grad():
+        net = Net()
+        net.load_state_dict(torch.load('./distribution_net.pth'))
+        net.to(device)
+        test_output = net(Test_d)
+        test_output = test_output.to("cpu", torch.double)
+        test_output = test_output.view(3, 31)
+        test_output_np = test_output.detach().numpy()
+        Learning_Plot(ur, vr, wr, test_output_np, 'Test')
 
 ################################################################################
 if __name__ == '__main__':
