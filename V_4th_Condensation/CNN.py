84004d51fe4aa737ee09fc463c2a68fa104ec05d
Khalil Wong <1039906090@qq.com>
2020-06-18 20:05:43 +0800

    2nd day finished!

--- V_4th_Condensation/CNN.py
+++ V_4th_Condensation/CNN.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
+#import numba as nb
+import matplotlib.pyplot as mpl
 
 ################################################################################
 def ReadFile(FileName):
@@ -11,8 +14,8 @@ def ReadFile(FileName):
         InData = Data[1:]
         ID = []
         Tt = []
-        CN = []
-        TmVt = []
+        #CN = []
+        #TmVt = []
         X = []
         Y = []
         Z = []
@@ -27,11 +30,11 @@ def ReadFile(FileName):
         FVZ = []
         Adsorbed = []
         for pdata in InData:
-            (id, tt, cn, tmvt, x, y, z, vx, vy, vz, fx, fy, fz, fvx, fvy, fvz, ad) = pdata.split('\t', 16)
+            (id, tt, x, y, z, vx, vy, vz, fx, fy, fz, fvx, fvy, fvz, ad) = pdata.split('\t', 14)
             ID.append(int(id))
             Tt.append(int(tt))
-            CN.append(int(cn))
-            TmVt.append(int(tmvt))
+            #CN.append(int(cn))
+            #TmVt.append(int(tmvt))
             X.append(float(x))
             Y.append(float(y))
             Z.append(float(z))
@@ -50,8 +53,8 @@ def ReadFile(FileName):
                 Adsorbed.append(1)
     ID = np.array(ID)
     Tt = np.array(Tt)
-    CN = np.array(CN)
-    TmVt = np.array(TmVt)
+    #CN = np.array(CN)
+    #TmVt = np.array(TmVt)
     X = np.array(X)
     Y = np.array(Y)
     Z = np.array(Z)
@@ -66,7 +69,7 @@ def ReadFile(FileName):
     FVZ = np.array(FVZ)
     Adsorbed = np.array(Adsorbed)
     #
-    return(ID, Tt, CN, TmVt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed)
+    return(ID, Tt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed)
 
 ################################################################################
 class Net(nn.Module):
@@ -74,13 +77,14 @@ class Net(nn.Module):
     def __init__(self):
         #
         super().__init__()
-        self.conv1 = nn.Conv1d(3, 8, 5)
-        self.conv2 = nn.Conv1d(8, 32, 5)
-        self.conv3 = nn.Conv1d(32, 128, 5)
+        self.conv1 = nn.Conv1d(3, 6, 1000)
+        self.conv2 = nn.Conv1d(6, 9, 1000)
+        self.conv3 = nn.Conv1d(9, 12, 1000)
+        self.conv4 = nn.Conv1d(12, 15, 1000)
         #
-        self.fc1 = nn.Linear(128 * 5 * 5, 2048)
-        self.fc2 = nn.Linear(2048, 1024)
-        self.fc3 = nn.Linear(1024, 31 * 31 * 3)
+        self.fc1 = nn.Linear(79695, 4096)
+        self.fc2 = nn.Linear(4096, 1024)
+        self.fc3 = nn.Linear(1024, 31 * 3)
         #
         #self.conv1 = nn.Conv1d(1, 3, 5)
 
@@ -89,6 +93,7 @@ class Net(nn.Module):
         x = F.max_pool1d(F.relu(self.conv1(x)), 2)
         x = F.max_pool1d(F.relu(self.conv2(x)), 2)
         x = F.max_pool1d(F.relu(self.conv3(x)), 2)
+        x = F.max_pool1d(F.relu(self.conv4(x)), 2)
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -103,7 +108,7 @@ class Net(nn.Module):
         return num_features
 
 ################################################################################
-@nb.jit(nopython = True, nogil = True)
+#@nb.jit(nopython = True, nogil = True)
 def Reflection_Distribution(UnA_N, X, Adsorbed, I, XL = 0.0, XH = 0.0):
     #
     if XL != XH:
@@ -127,10 +132,52 @@ def Reflection_Distribution(UnA_N, X, Adsorbed, I, XL = 0.0, XH = 0.0):
     return(x, f)
 
 ################################################################################
+#@nb.jit(nopython = True, nogil = True)
+def Learning_Plot(ur, vr, wr, output, Type):
+    #
+    d, N = output.shape
+    #
+    for i in range(d - 1):
+        fig, ax = mpl.subplots()
+        #levels = np.arange(0.0, 0.75, 0.05)
+        #mpl.contourf(ur, ui, output[:, :, i], levels)
+        #mpl.colorbar()
+        ax.set_xlim(-2.0, 2.0)
+        ax.set_ylim(0.0, 0.7)
+        if i == 0:
+            ax.plot(ur, output[i, :], 'ro', markersize = 3)
+            ax.set_xlabel('$u_{r}$')
+            ax.set_ylabel('$f$')
+            #ax.set_title('p0 = %f;p1 = %f'%(p[0], p[1]))
+            mpl.savefig('CNN_u_'+ Type +'.png', dpi = 600)
+        else:
+            ax.plot(vr, output[i, :], 'ro', markersize = 3)
+            ax.set_xlabel('$v_{r}$')
+            ax.set_ylabel('$f$')
+            #ax.set_title('p0 = %f;p1 = %f'%(p[0], p[1]))
+            mpl.savefig('CNN_v_'+ Type +'.png', dpi = 600)
+        mpl.show()
+        #mpl.close()
+    #
+    fig, ax = mpl.subplots()
+    #levels = np.arange(0.0, 1.05, 0.05)
+    #mpl.contourf(ui[:, 1], wr[:, 1], output[:, :, 2], levels)
+    #mpl.colorbar()
+    ax.plot(wr, output[2, :], 'ro', markersize = 3)
+    ax.set_xlim(0.0, 2.0)
+    ax.set_ylim(0.0, 1.0)
+    ax.set_xlabel('$w_{r}$')
+    ax.set_ylabel('$f$')
+    #ax.set_title('p0 = %f;p1 = %f'%(p[0], p[1]))
+    mpl.savefig('CNN_w_'+ Type +'.png', dpi = 600)
+    mpl.show()
+    #mpl.close()
+
+################################################################################
 def main():
     #
     FileName = 'Incident_Reflection.data'
-    ID, Tt, CN, TmVt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed = ReadFile(FileName)
+    ID, Tt, X, Y, Z, VX, VY, VZ, FX, FY, FZ, FVX, FVY, FVZ, Adsorbed = ReadFile(FileName)
     UnA_N = 0
     for i in range(len(Adsorbed)):
         if Adsorbed[i] == 0:
@@ -141,10 +188,11 @@ def main():
     Training = Training.view(1, 3, -1)
     #
     I = 30
-    Targetx = Reflection_Distribution(UnA_N, FVX, Adsorbed, I, -2.0, 2.0)
-    Targety = Reflection_Distribution(UnA_N, FVY, Adsorbed, I, -2.0, 2.0)
-    Targetz = Reflection_Distribution(UnA_N, FVZ, Adsorbed, I, 0.0, 2.0)
-    Target_np = np.concatenate((Targetx, Targety, Targetz), axis = 0)
+    ur, Targetx = Reflection_Distribution(UnA_N, FVX, Adsorbed, I, -2.0, 2.0)
+    vr, Targety = Reflection_Distribution(UnA_N, FVY, Adsorbed, I, -2.0, 2.0)
+    wr, Targetz = Reflection_Distribution(UnA_N, FVZ, Adsorbed, I, 0.0, 2.0)
+    Target_np = np.concatenate((Targetx, Targety, Targetz), axis = 0).reshape((3, 31))
+    Learning_Plot(ur, vr, wr, Target_np, 'Target')
     Target = torch.from_numpy(Target_np)
     Target = Target.view(1, -1)
     #
@@ -152,21 +200,28 @@ def main():
     print(device)
     net = Net()
     net.to(device)
-    Training_d = Training.to(device)
-    Target_d = Target.to(device)
-    criterion = nn.CrossEntropyLoss()
+    Training_d = Training.to(device, dtype=torch.float)
+    Target_d = Target.to(device, dtype=torch.float)
+    criterion = nn.MSELoss()
     optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.9)
     #
     for i in range(1000):
+        print(i)
         # zero the parameter gradients
         optimizer.zero_grad()
         # forward + backward + optimize
         Prediction = net(Training_d)
-        loss = criterion(Prediction, Target)
+        loss = criterion(Prediction, Target_d)
         print(loss.item())
         loss.backward()
+        #print(torch.cuda.memory_summary())
         optimizer.step()
     #
     output = Prediction.to("cpu", torch.double)
-    output = output.view(31, 31, 3)
-    Learning_Plot(output)
+    output = output.view(3, 31)
+    output_np = output.detach().numpy()
+    Learning_Plot(ur, vr, wr, output_np, 'Prediction')
+
+################################################################################
+if __name__ == '__main__':
+    main()
